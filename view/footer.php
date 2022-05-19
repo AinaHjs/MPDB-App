057c85349d369a748a1bb8435e086bebd46411c1
AinaHjs <andriamiadantsoaaina@gmail.com>
2022-05-19 14:59:27 +0100

    version refaite

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,19 @@
-<footer>
-        <div class="flex">
-            <p>MPDB Application by Aina Andriamiadantsoa</p>
+<footer id="footer">
+        <div class="container-fluid">
+            <div class="row footer">
+                <div class="col-4 settings">
+                    <p>Setting and Privacy</p>
+                </div>
+                <div class="col-4 socialNetwork">
+                    <P>Social network</P>
+                </div>
+                <div class="col-4 Aina">
+                    <p><a href="#">MPDB</a> an application developped by <a href="http://ainahjs.github.io">A-SOFT-IT</a>.</p>
+                </div>
+            </div>
         </div>
     </footer>
-    <script src="lib/js/bootstrap.bundle.js"></script>
+<script src="lib/js/bootstrap.bundle.js"></script>
+<script src="main.js"></script>
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="lib/css/bootstrap.css">
     <link rel="stylesheet" href="css/main.css">
-    <title>MPDB Application</title>
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+
+    <title>MPDB application</title>
 </head>
 <body>
-    <main>
-        <div class="container">
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-8">
-                    <header>
-                        <h1 class="text-center">MPDB Application</h1>
-                    </header>
+    <!-- Header section -->
+    <header id="header">
+        <div class="container-fluid">
+            <div class="row header">
+                <div class="col-4 brand">
+                    <h1>MPDB</h1>
+                </div>
+                <div class="col-8 navigationBar">
+                    <p>a php application by <a href="http://ainahjs.github.io" target="_blank">A-SOFT-IT</a>.</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- / Header section -->
